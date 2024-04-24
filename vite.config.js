94b5d09bc52af3508492d82eb8aeb5e8a164d256
Juan Delgado <delgadojuan579@gmail.com>
2024-04-23 17:29:15 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './', // Set the base path to relative
+  build: {
+    outDir: 'dist', // Output directory
+    assetsDir: '.', // Relative path to assets directory
+  }
 });
